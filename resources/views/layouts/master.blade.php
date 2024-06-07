d117f9636ab6fee27cf1eac0a973d9540c4ebd99
Tim S <info@nsystemz.de>
2024-06-07 15:27:07 +0200

    Update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Nemesus World | User Control Panel by Nemesus.de</title>
     <link rel="icon" type="image/png" href="{{asset('images/favicon.ico')}}" />
