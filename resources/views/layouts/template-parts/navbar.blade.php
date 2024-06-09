c7df7ae5c8e03f6b0cc7305d078d7b7a8c297fc1
Tim S <info@nsystemz.de>
2024-06-09 10:33:42 +0200

    Update

--- resources/views/layouts/template-parts/navbar.blade.php
+++ resources/views/layouts/template-parts/navbar.blade.php
@@ -1,20 +1,11 @@
-<?php
-    $updateCheck = FunctionsController::checkForUCPUpdate();
-?>
+
 <nav class="main-header navbar navbar-expand navbar-white navbar-light bg-primary" style="color:white">
     <ul class="navbar-nav">
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
     </ul>
-    @if ($updateCheck == false)
-        <marquee><span class="ml-2">Viel Spass mit dem UCP - Nemesus.de -
-                {{ strftime('%d %b. %Y - %H:%M:%S', time()) }}</span></marquee>
-    @else
-        <marquee><span class="ml-2">Es steht ein neues <a
-                    href="https://github.com/nSystemz/nemesus-world-ucp/releases">UCP Update</a zur Verfügung, bitte
-                    updaten!</span></marquee>
-    @endif
+    <marquee><span class="ml-2">Viel Spass mit dem UCP - Nemesus.de - {{ strftime('%d %b. %Y - %H:%M:%S', time()) }}</span></marquee
     <ul class="navbar-nav ml-auto">
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
