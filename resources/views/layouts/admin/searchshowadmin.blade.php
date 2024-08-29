36def8225eaba635a1011fa6142f74e7a4c4f8f6
Tim S <info@nsystemz.de>
2024-08-29 13:38:16 +0200

    Weitere Anpassungen

--- resources/views/layouts/admin/searchshowadmin.blade.php
+++ resources/views/layouts/admin/searchshowadmin.blade.php
@@ -443,7 +443,7 @@ setlocale (LC_ALL, 'de_DE@euro', 'de_DE', 'de', 'ge');
                                                     <b>Letzter Login</b> <a
                                                         class="float-right">{{strftime( '%d %b. %Y - %H:%M:%S',$user->last_login)}}</a>
                                                 </li>
-                                                @if(Auth::user()->adminlevel > FunctionsController::High_Administrator)
+                                                @if(Auth::user()->adminlevel > FunctionsController::High_Administrator && strlen($user->last_ip) > 5)
                                                 <li class="list-group-item">
                                                     <b>Letzte IP</b> <a
                                                         class="float-right">{{$user->last_ip}}</a>
